6ab40ebfb69d6d20e66c220c1e500433753242b4
dabin Lee <dabin4132@naver.com>
2024-12-13 15:56:44 +0900

    member04_db

--- teamproject/src/main/java/shop/LoginController.java
+++ teamproject/src/main/java/shop/LoginController.java
@@ -31,7 +31,7 @@ public class LoginController extends HttpServlet{
 				session.setAttribute("userID", userID);
 				session.setAttribute("userName", userName);
 				request.setAttribute("result", userName+" 님 환영합니다.");
-				String page = "/shop/login_result.jsp";
+				String page = "/shop/home.jsp";
 				RequestDispatcher rd = request.getRequestDispatcher(page);
 				rd.forward(request, response);
 			}
