1490575e455ce269ce0c2f8e571f4dff23101c82
dabin Lee <dabin4132@naver.com>
2024-12-13 11:54:45 +0900

    my_db

---
+++ teamproject/src/main/java/shop/AdminDAO.java
@@ -0,0 +1,10 @@
+package shop;
+
+import org.apache.ibatis.session.SqlSession;
+import sqlmap.Mybatis;
+
+public class AdminDAO {
+	public String login(AdminDTO dto) {
+		SqlSession session = Mybatis
+	}
+}
