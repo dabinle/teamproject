2bad0c1eb9337dfe9ebb9da507c41f00404f7fd4
eunkyung <dbdmsrud02@naver.com>
2024-12-23 18:05:37 +0900

    dd

---
+++ teamproject/src/main/java/board/UploadController.java
@@ -0,0 +1,30 @@
+package board;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.MultipartConfig;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.Part;
+
+@MultipartConfig(maxFileSize = 1024 * 1024 * 10, location = "c:/upload")
+public class UploadController extends HttpServlet {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<String> file_names = new ArrayList<>();
+		List<Long> file_sizes = new ArrayList<>();
+		try {
+			for (Part part : request.getParts()) {
+				String noticeFile = part.getSubmittedFileName();
+				if (noticeFile != null) {
+					file_names.add(noticeFile)
+				}
+			}
+		}
+		
+	}
+}
