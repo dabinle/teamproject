88dcb1958cdc95afff5fb2a983b9b0b220a6856a
dabin Lee <dabin4132@naver.com>
2024-12-30 13:08:13 +0900

    faq_db

--- teamproject/src/main/java/faq/FaqDAO.java
+++ teamproject/src/main/java/faq/FaqDAO.java
@@ -1,6 +1,7 @@
 package faq;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.apache.ibatis.session.SqlSession;
@@ -15,7 +16,143 @@ public class FaqDAO {
 			Map<String, Object> map = new HashMap<>();
 			map.put("search_option", search_option);
 			map.put("keyword", keyword);
-			result = session.selectOne("", map)
+			result = session.selectOne("faq.search_count", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
 		}
+		return result;
+	}
+	
+	public List<FaqDTO> list_search(String search_option, String keyword, int start, int end){
+		List<FaqDTO> list = null;
+		SqlSession session = Mybatis.getInstance().openSession();
+		try {
+			Map<String, Object> map = new HashMap<>();
+			map.put("search_option", search_option);
+			map.put("keyword", keyword);
+			map.put("start", start);
+			map.put("end", end);
+			list = session.selectList("faq.search_list", map);
+			
+			for (FaqDTO dto : list) {
+				String adminId = dto.getAdminId();
+				String qusetion = dto.getQusetion();
+				String f_categoryName = dto.getF_categoryName();
+				
+				switch(search_option) {
+				case "all":
+					adminId = adminId.replace(keyword, "<span style='color:red'>" + keyword + "</span>");
+					qusetion = qusetion.replace(keyword, "<span style='color:red'>" + keyword + "</span>");
+					f_categoryName = f_categoryName.replace(keyword, "<span style='color:red'>" + keyword + "</span>");
+					break;
+				case "qusetion":
+					qusetion = qusetion.replace(keyword, "<span style='color:red'>" + keyword + "</span>");
+					break;
+				case "f_categoryName":
+					f_categoryName = f_categoryName.replace(keyword, "<span style='color:red'>" + keyword + "</span>");
+					break;
+				}
+				dto.setAdminId(adminId);
+				dto.setQusetion(qusetion);
+				dto.setF_categoryName(f_categoryName);
+			} 
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+		return list;
+	}
+	
+	public FaqDTO detailFaq(int faqNum) {
+		SqlSession session = Mybatis.getInstance().openSession();
+		FaqDTO dto = session.selectOne("faq.detail_faq", faqNum);
+		session.close();
+		return dto;
+	}
+	
+	public void update(FaqDTO dto) {
+		SqlSession session = null;
+		try {
+			session = Mybatis.getInstance().openSession();
+			session.update("faq.update", dto);
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+	}
+	
+	public void delete(int faqNum) {
+		SqlSession session = null;
+		try {
+			session = Mybatis.getInstance().openSession();
+			session.delete("faq.delete", faqNum);
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+	}
+	public List<FaqDTO> list(int pageStart, int PageEnd){
+		List<FaqDTO> list = null;
+		SqlSession session = null;
+		try {
+			session = Mybatis.getInstance().openSession();
+			Map<String, Object> map = new HashMap<>();
+			map.put("start", pageStart);
+			map.put("end", PageEnd);
+			list = session.selectList("faq.list", map);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+		return list;
+	}
+	
+	public void insert(FaqDTO dto) {
+		SqlSession session = null;
+		try {
+			session = Mybatis.getInstance().openSession();
+			session.insert("faq.insert", dto);
+			session.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+	}
+	
+	public FaqDTO view(int faqNum) {
+		FaqDTO dto = null;
+		SqlSession session = null;
+		try {
+			session = Mybatis.getInstance().openSession();
+			dto = session.selectOne("faq.detail_faq", faqNum);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(session != null)
+				session.close();
+		}
+		return dto;
+	}
+	
+	public List<FapCategoryDTO> listf_category(){
+		SqlSession session = Mybatis.getInstance().openSession();
+		List<FapCategoryDTO> f_category = session.selectList("faq.list_f_category");
+		session.close();
+		return f_category;
 	}
 }
--- teamproject/src/main/java/faq/FaqDTO.java
+++ teamproject/src/main/java/faq/FaqDTO.java
@@ -1,9 +1,17 @@
 package faq;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class FaqDTO {
 	private int faqNum;
 	private int f_categoryNum ;
 	private String adminId;
 	private String qusetion;
 	private String f_answer;
+	private String f_categoryName;
 }
